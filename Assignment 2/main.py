41bcd6c2cce496ed887cd5e123345ed75c3b8856
UmaEriksson <45813476+UmaEriksson@users.noreply.github.com>
2024-01-31 15:23:57 +0100

    Update main.py

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 canvas_width = canvas_height = 500
-canvas_padding = 7
+canvas_padding = 20
 
 x_axis_start = (0, canvas_height/2)
 x_axis_end = (canvas_width, canvas_height/2)
@@ -11,33 +11,63 @@ x_axis_end = (canvas_width, canvas_height/2)
 y_axis_start = (canvas_width/2, 0)
 y_axis_end = (canvas_width/2, canvas_height)
 
-data = pd.read_csv('data1.csv', header=None)
+data = pd.read_csv('data2.csv', header=None)
 
 x_values = data.get(0)
 y_values = data.get(1)
 type_values = data.get(2)
 
-output_x = np.array(np.interp(x_values, [min(x_values), max(x_values)], [0, canvas_width]))
+output_x = np.array(
+    np.interp(x_values, [min(x_values), max(x_values)], [0, canvas_width]))
 print(output_x.astype(int))
 
-output_y = np.array(np.interp(y_values, [min(y_values), max(y_values)], [canvas_height, 0]))
+output_y = np.array(
+    np.interp(y_values, [min(y_values), max(y_values)], [canvas_height, 0]))
 print(output_y.astype(int))
 
+def onItemClick(event, x, y):
+   print(x, y)
+  
+
+
+ 
 
 def create_shape(canvas: tk.Canvas, x: int, y: int, r: float, type_value: str):
     x = canvas_padding + x
+    y = canvas_padding + y
     match type_value:
         case 'a':
-            canvas.create_oval(x - r, y - r, x + r, y + r, fill="black", outline=None)
-            
+            object = canvas.create_oval(
+                x - r, y - r, x + r, y + r, fill="black", outline=None)
+            canvas.tag_bind(object, '<Button-1>', lambda e: onItemClick(e, x, y), tags="obj1Tag")
+
         case 'b':
-            canvas.create_rectangle(x - r, y - r, x + r, y + r, fill="black", outline=None)
+            cbject = canvas.create_rectangle(
+                x - r, y - r, x + r, y + r, fill="black", outline=None)
+            #object.tag_bind(object, '<Button-1>', onItemClick)
 
         case 'c':
-            canvas.create_polygon([x, y + r + 0.5, x - r - 0.5, y - r - 0.5, x + r + 0.5, y - r - 0.5], fill="black", outline=None)
+            object = canvas.create_polygon(
+                [x, y + r + 0.5, x - r - 0.5, y - r - 0.5, x + r + 0.5, y - r - 0.5], fill="black", outline=None)
+            #object.tag_bind(object, '<Button-1>', onItemClick)
+
+        case 'foo':
+            object = canvas.create_oval(
+                x - r, y - r, x + r, y + r, fill="black", outline=None)
+            canvas.tag_bind(object, '<Button-1>', lambda e: onItemClick(e, x, y))
+
+        case 'bar':
+            object = canvas.create_rectangle(
+                x - r, y - r, x + r, y + r, fill="black", outline=None)
+            #object.tag_bind(object, '<Button-1>', onItemClick)
+
+        case 'baz':
+            object = canvas.create_polygon(
+                [x, y + r + 0.5, x - r - 0.5, y - r - 0.5, x + r + 0.5, y - r - 0.5], fill="black", outline=None)
+            #object.tag_bind(object, '<Button-1>', onItemClick)
 
 
-#Create window
+# Create window
 window = tk.Tk()
 window.geometry("500x500")
 
@@ -49,7 +79,7 @@ canvas.create_line(canvas_width/2, 0, canvas_width/2, canvas_height)
 # Create circles for every point and sets color according to type 'a', 'b' or 'c'
 for i in range(len(output_x) - 1):
     create_shape(canvas, output_x[i], output_y[i], 3, type_values[i])
-    
+
 # create ticks on the x-axis
 
 # for i in range(canvas_width):
@@ -60,7 +90,7 @@ for i in range(len(output_x) - 1):
 #         canvas.create_text(i, canvas_height/2 + 10, text=str(round(np.interp(i, [0, canvas_width], [min(x_values), max(x_values)]))))
 
 
-#     if i % 20 == 0:        
+#     if i % 20 == 0:
 #         canvas.create_line(i, canvas_height/2 - 4, i, canvas_height/2 + 4)
 
 # # create ticks on the y-axis
@@ -70,17 +100,24 @@ for i in range(len(output_x) - 1):
 
 #     if i % 40 == 0:
 #         canvas.create_text(canvas_width/2 + 12, i, text=str(round(np.interp(i, [0, canvas_height], [max(y_values), min(y_values)]))))
-   
+
 #     if i % 20 == 0:
 #         canvas.create_line(canvas_width/2 - 4, i, canvas_width/2 + 4, i)
-    
-for i in range(min(x_values), max(x_values)):
+
+for i in range(len(x_values)):
     if i % 10 == 0:
-        canvas.create_line(round(np.interp(i, [min(x_values), max(x_values)], [0, canvas_width])) + canvas_padding, canvas_height/2 - 2, round(np.interp(i, [min(x_values), max(x_values)], [0, canvas_width])) + canvas_padding, canvas_height/2 + 3)
-        canvas.create_text(round(np.interp(i, [min(x_values), max(x_values)], [0, canvas_width])) + canvas_padding, canvas_height/2 + 10, text=str(i))
+        canvas.create_line(round(np.interp(i, [min(x_values), max(x_values)], [0, canvas_width])) + canvas_padding, canvas_height/2 - 2, round(
+            np.interp(i, [min(x_values), max(x_values)], [0, canvas_width])) + canvas_padding, canvas_height/2 + 3)
+        canvas.create_text(round(np.interp(i, [min(x_values), max(x_values)], [
+                           0, canvas_width])) + canvas_padding, canvas_height/2 + 10, text=str(i))
+
+for i in range(len(y_values)):
+    if i % 10 == 0:
+        canvas.create_line(canvas_width/2 - 2, round(np.interp(i, [min(y_values), max(y_values)], [0, canvas_height])) + canvas_padding,
+                           canvas_width/2 + 3, round(np.interp(i, [min(y_values), max(y_values)], [0, canvas_height])) + canvas_padding)
+        canvas.create_text(canvas_width/2 + 10, round(np.interp(
+            i, [min(y_values), max(y_values)], [canvas_height, 0])) + 3, text=str(i))
 
 canvas.place(relx=0.5, rely=0.5, anchor="center")
-#start mainloop
+# start mainloop
 window.mainloop()
-
-
