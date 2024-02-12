d7ae10ebd0be4924aa1d5f80bdc0fed08d3e05a9
Hugo Dahl <hugodahl99@hotmail.com>
2024-02-12 13:40:13 +0100

    setup for lab 4

---
+++ Assignment 4/assignment4/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  
+
+  return (
+    <>  
+      <ChakraProvider>
+        {/* Rest of the code */}
+      </ChakraProvider>
+    </>
+  )
+}
+
+export default App
